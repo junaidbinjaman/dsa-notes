1efd74702a9492695a1e1c2bfea868753dff6f6c
junaidbinjaman <me@junaidbinjaman.com>
2023-12-03 10:01:10 +0600

    dsa: added a shift ddl method

--- dsa-in-js-01-11-2025/DLL.js
+++ dsa-in-js-01-11-2025/DLL.js
@@ -63,8 +63,25 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    shift() {
+        if (this.length === 0) return undefined;
+        const temp = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            temp.next = null;
+        }
+
+        this.length--;
+        return temp;
+    }
 }
 
 const dll = new DoublyLinkedList(1);
 dll.push(2);
-dll.push(3);
+
