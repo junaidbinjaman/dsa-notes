a98a43fa3c1c5d9b98bfca9511f85e779b20ed76
junaidbinjaman <me@junaidbinjaman.com>
2023-12-08 10:10:01 +0600

    problem-solving: Solved maxSubarraySum using sliding window technique

--- leetcode-problem-solving/maxSubarraySum.js
+++ leetcode-problem-solving/maxSubarraySum.js
@@ -18,19 +18,53 @@ function maxSubarraySum(arr, k) {
 
 // console.log(maxSubarraySum([2, 1, 5, 1, 3, 2], 3)); // Output: 9
 
-function maxSubarraySum1(arr, k) {
-    let maxSub = 0, windowSub = 0;
-
-    for (let i = 0; i < k; i++) {
-        windowSub += arr[i];
+/**
+ * Given an array of integers and a number,
+ * write a function called maxSubarraySum,
+ * which finds the maximum sum of a subarray with the length of the number passed to the function.
+ * 
+ * Note that a subarray must consist of consecutive elements from the original array.
+ * In the first example below, [100, 200, 300] is a subarray of the original
+ */
+function maxSubarraySum(arr, num) {
+    // 📋 Check if the array is empty or the number is bigger than the array
+    if (!arr.length || num > arr.length - 1) {
+        return null;
     }
 
-    for (let i = k; i < arr.length; i++) {
-        windowSub += arr[i] - arr[i - k];
-        maxSub = Math.max(windowSub, maxSub);
+    // 📋 Define the maxSum and windowSum;
+    let maxSum = 0, windowSum = 0;
+
+    // 📋 Loop until num and create the windowSum
+    for (let i = 0; i < num; i++) {
+        windowSum += arr[i];
     }
 
-    return maxSub;
+    // 📋 Add window sum into the maxSum
+    maxSum = windowSum;
+
+    // 📋 Loop over the array starting from num
+    for (let i = num; i < arr.length; i++) {
+        // 📋 Add one element from the right and abstract one from the left
+        windowSum += arr[i] - arr[i - num]
+
+        // 📋 Replace maxSum with windowSum is windowSum is greater than maxSum
+        maxSum = Math.max(windowSum, maxSum);
+    }
+
+    // 📋 Return the maxSum
+    return maxSum;
 }
 
-console.log(maxSubarraySum1([2, 1, 5, 1, 3, 2], 3));
+// console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+// console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
+// console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+// console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+console.log(maxSubarraySum([2,3], 3)) // null
+
+// Problem solving approaches.
+// 🔷 Understand the problem.
+// 🔷 Concrete example.
+// 🔷 Break it down.
+// 🔷 Solve & simplify.
+// 🔷 Look back & refactor.
