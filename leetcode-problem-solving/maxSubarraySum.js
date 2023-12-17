451d0be48bad043978100196201106f11ce6166a
junaidbinjaman <me@junaidbinjaman.com>
2023-12-17 10:10:01 +0600

    problem-solving: Solved max sub array sum problem using naive approach. The time complexity is O(n^2)

---
+++ leetcode-problem-solving/maxSubarraySum.js
@@ -0,0 +1,19 @@
+// Problem Description: Find the maximum sum of a subarray of size k.
+// This is naive approach.
+function maxSubarraySum(arr, k) {
+    let maxSum = -Infinity;
+
+    for (let i = 0; i <= arr.length - k; i++) {
+        let sum = 0;
+
+        for (let j = i; j < i + k; j++) {
+            sum += arr[j]
+        }
+
+        maxSum = Math.max(maxSum, sum);
+    }
+
+    return maxSum;
+}
+
+console.log(maxSubarraySum([2, 1, 5, 1, 3, 2], 3)); // Output: 9
