b5d83a71c164be4d5cf567de4fa87dfaa9fe911e
junaidbinjaman <me@junaidbinjaman.com>
2023-12-21 10:10:01 +0600

    feat: added SLL remove method

--- dsa-in-js-01-11-2025/LinkedList.js
+++ dsa-in-js-01-11-2025/LinkedList.js
@@ -119,6 +119,20 @@ class LinkedList {
         return true;
     }
 
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length) return this.pop();
+
+        const before = this.get(index - 1);
+        const temp = before.next;
+
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
+
     
 }
 
