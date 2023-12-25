3e0e0f12444badca26b71b055f9871438d89e524
junaidbinjaman <me@junaidbinjaman.com>
2023-12-25 10:10:01 +0600

    feat: added SLL get method

--- dsa-in-js-01-11-2025/LinkedList.js
+++ dsa-in-js-01-11-2025/LinkedList.js
@@ -82,6 +82,18 @@ class LinkedList {
         return temp;
     }
 
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined;
+
+        let temp = this.head;
+
+        for(let i = 0; i < 0; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+
     
 }
 
