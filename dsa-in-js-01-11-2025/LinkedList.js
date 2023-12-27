bdd03baaf44e93199aa1d945bfa3829903e5af18
junaidbinjaman <me@junaidbinjaman.com>
2023-12-27 10:10:01 +0600

    feat: added SLL unshift method

--- dsa-in-js-01-11-2025/LinkedList.js
+++ dsa-in-js-01-11-2025/LinkedList.js
@@ -52,6 +52,21 @@ class LinkedList {
         return temp;
     }
 
+    unshift(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
     
 }
 
