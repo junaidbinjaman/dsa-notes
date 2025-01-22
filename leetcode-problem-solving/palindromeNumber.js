ce2226aca187fa30e96754ef8355de7fcfb44f28
junaidbinjaman <me@junaidbinjaman.com>
2025-01-22 10:01:10 +0600

    problem-solving: Solved palindrome number problem using two pointer technique.

---
+++ leetcode-problem-solving/palindromeNumber.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x = x.toString()
+    let left = 0, right = x.length - 1;
+
+    while(left < right) {
+        if (x[left] !== x[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+};
+
+const num = 10;
+
+console.log(isPalindrome(num));
