b987bf2f240c6a90d62d4a92276c472d6e41f06a
junaidbinjaman <me@junaidbinjaman.com>
2025-01-28 10:10:11 +0600

    dsa: written depth-first search inorder method on tree class to traversal the tree

--- dsa-in-js-01-11-2025/algorithms/bfs.js
+++ dsa-in-js-01-11-2025/algorithms/bfs.js
@@ -163,6 +163,20 @@ class BST {
         return results;
     }
 
+    DFSInOrder() {
+        let results = [];
+
+        function traverse(currentNode) {
+            if (currentNode.left) traverse(currentNode.left);
+            results.push(currentNode.value);
+            if (currentNode.right) traverse(currentNode.right);
+        }
+        traverse(this.root);
+
+        return results;
+
+    }
+
 }
 
 const myTree = new BST();
