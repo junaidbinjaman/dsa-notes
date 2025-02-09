a6f5d20e5217c0d274f42ab0f69611e579a03a4d
junaidbinjaman <me@junaidbinjaman.com>
2025-02-09 07:36:34 +0600

    dsa: written well optimized bubble sort in js

--- dsa-in-js-01-11-2025/algorithms/bubbleSort.js
+++ dsa-in-js-01-11-2025/algorithms/bubbleSort.js
@@ -1,17 +1,22 @@
-function bubbleSort (arr) {
-    
-    for (let i = arr.length - 1; i > 0; i--) {
-        for (let j = 0; j < i; j++) {
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swap;
+
+    do {
+        swap = false;
+        for (let j = 0; j < n - 1; j++) {
             if (arr[j] > arr[j+1]) {
-                let temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+                swap = true;
             }
         }
-    }
-    
+        n--
+    } while (swap)
 
     return arr;
 }
 
-bubbleSort([12, 6, 11, 50, 32])
+console.log(bubbleSort([12, 6, 11, 50, 32])); // Output: [6, 11, 12, 32, 50]
+// console.log(bubbleSort([1, 2, 3, 4, 5]));      // Output: [1, 2, 3, 4, 5] (Early exit)
+// console.log(bubbleSort([5, 4, 3, 2, 1]));      // Output: [1, 2, 3, 4, 5] (Worst-case)
+
