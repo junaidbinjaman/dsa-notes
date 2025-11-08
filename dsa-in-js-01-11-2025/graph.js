ce6102dd6a383efd9bc76bbe4a73cc24a2423b68
junaidbinjaman <me@junaidbinjaman.com>
2025-11-08 10:01:10 +0600

    dsa: Written graph in js.

---
+++ dsa-in-js-01-11-2025/graph.js
@@ -0,0 +1,16 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+            return true;
+        }
+        return false;
+    }
+}
+
+const myGraph = new Graph();
+myGraph;
